361ffd36cbc3e52d33dfc077b4c75126316ecc78
Alex de Kruijff <alex.de.kruijff@mazarineblue.org>
2026-02-12 18:57:37 +0100

    Project 02 - Reverse String

---
+++ 02 - Reverse String/ReverseStringProgram.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+
+namespace _02___Reverse_String
+{
+    internal class ReverseStringProgram
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+                Console.WriteLine("Usage: Program <string1> [<string2>...]");
+            else
+                for (int i = 0; i < args.Length; ++i)
+                {
+                    String s = args[i].ToLower();
+                    String r = ReverseString(s);
+                    if (IsPalindrome(s, r))
+                        Console.WriteLine($"{s} - {r} - is a palindrome");
+                    else
+                        Console.WriteLine($"{s} - {r} - is not a palindrome");
+                }
+        }
+
+        private static string ReverseString(string s)
+        {
+            String r = "";
+            for (int i = 0; i < s.Length; ++i)
+                r = s[i] + r;
+            return r;
+        }
+
+        private static bool IsPalindrome(string l, string r)
+        {
+            if (l.Length != r.Length)
+                return false;
+            for (int i = 0; i < l.Length; ++i)
+                if (l[i] != r[i])
+                    return false;
+            return true;
+        }
+    }
+}
