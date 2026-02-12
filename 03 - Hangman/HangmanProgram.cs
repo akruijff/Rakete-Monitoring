dc277e472b00ee383d1b2864ac7952326df6ea29
Alex de Kruijff <alex.de.kruijff@mazarineblue.org>
2026-02-12 21:39:09 +0100

    Refactored Main into smaller methods

--- 03 - Hangman/HangmanProgram.cs
+++ 03 - Hangman/HangmanProgram.cs
@@ -20,22 +20,31 @@
 
             while (!game.IsGameOver())
             {
-                Console.Clear();
-                Console.WriteLine("Welcome to Hangman!");
-                Console.WriteLine();
-                Console.WriteLine(game.Status());
-                Console.WriteLine();
-
-                Console.WriteLine("Guess a letter: ");
-                game.Guess(Console.ReadKey().KeyChar);
+                DisplayStatus(game);
+                GuessLetter(game);
             }
 
+            DisplayStatus(game);
+            DisplayResult(game);
+        }
+
+        private static void DisplayStatus(Hangman game)
+        {
             Console.Clear();
             Console.WriteLine("Welcome to Hangman!");
             Console.WriteLine();
             Console.WriteLine(game.Status());
             Console.WriteLine();
+        }
 
+        private static void GuessLetter(Hangman game)
+        {
+            Console.WriteLine("Guess a letter: ");
+            game.Guess(Console.ReadKey().KeyChar);
+        }
+
+        private static void DisplayResult(Hangman game)
+        {
             if (game.IsGameWon())
                 Console.WriteLine("YOU WIN!");
             else
